2d891a3a1dc0eb506cedd61f742dd6015209ffeb
Jarron <jarronanthonipillai@gmail.com>
2022-11-19 18:32:23 -0500

    add comments to code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use bevy::prelude::*;
 
+//screen size
 pub const HEIGHT: f32 = 720.0;
 pub const WIDTH: f32 = 1280.0;
 
 fn main() {
+    //create the window
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
         .add_startup_system(spawn_camera)
@@ -21,6 +23,7 @@ fn main() {
         .run();
 }
 
+//generate a 3d camera
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
@@ -29,12 +32,15 @@ fn spawn_camera(mut commands: Commands) {
 }
 
 fn spawn_basic_scene(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+   
+    //create a flat plane
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
         material: materials.add(Color::rgb(0.1, 0.4, 0.7).into()),
         ..default()
     });
 
+    //create a cube
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
         material: materials.add(Color::rgb(0.67, 0.94, 0.42).into()),
